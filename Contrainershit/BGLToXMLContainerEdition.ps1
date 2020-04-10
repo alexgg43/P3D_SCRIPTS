0e01aa81ac28313d10f22eebba91dab9682cd16e
alexgg43 <alexgg@homtail.fr>
2020-04-10 15:41:38 +0200

    22

--- Contrainershit/BGLToXMLContainerEdition.ps1
+++ Contrainershit/BGLToXMLContainerEdition.ps1
@@ -3,33 +3,28 @@ $xmls = "c:\xmls"
 
 $bglToXmlPath = "C:\Bgl2Xml\Bgl2Xml.exe"
 
-$bglScenery = (Get-ChildItem $bgls -Filter "*.bgl" -Recurse | where-object {$_.length -gt 2KB})
+$bglScenery = (Get-ChildItem $bgls -Filter "*.bgl"<#  | where-object {$_.length -gt 1KB} #>)
 
 for ($i = 0; $i -lt $bglScenery.Count; $i++) {
-    $proc = Start-Process -filePath $bglToXmlPath -ArgumentList "`"$($bglScenery[$i].FullName)`" `"$xmls\$($bglScenery[$i].BaseName).xml`" `"no`""  -NoNewWindow -PassThru -RedirectStandardOutput NUL
+    try {
+        $er = (Invoke-Expression "$($bglToXmlPath) `"$($bglScenery[$i].FullName)`" `"$xmls\$($bglScenery[$i].BaseName).xml`" `"no`"") 2>&1
+        <# $proc = Start-Process -filePath $bglToXmlPath -ArgumentList "`"$($bglScenery[$i].FullName)`" `"$xmls\$($bglScenery[$i].BaseName).xml`" `"no`""  -NoNewWindow -PassThru -RedirectStandardOutput NUL -RedirectStandardError NUL
 
-    # keep track of timeout event
-    $timeouted = $null # reset any previously set timeout
-
-    # wait up to x seconds for normal termination
-    $proc | Wait-Process -Timeout 5 -ErrorAction SilentlyContinue -ErrorVariable $timeouted
-
-    if ($timeouted)
-    {
-        # terminate the process
-        Write-Host "toto"
-        $proc | kill
-
-        # update internal error counter
-    }
-    elseif ($proc.ExitCode -ne 0)
-    {
-        Write-Host "!!--ExitCode != 0--!!"
+        # wait up to x seconds for normal termination
+        $proc | Wait-Process -Timeout 60 -ErrorAction SilentlyContinue #-ErrorVariable $timeouted
+    
+        if ($proc.ExitCode -ne 0)
+        {
+            Write-Host "!!--ExitCode != 0--!!"
+        } #>
+    }catch{
+        Write-Host $PSItem.Exception.Message -ForegroundColor Red
     }
 
-    if(Test-Path -Path "$($xmls)\$($bglScenery[$i].BaseName).xml"){
-        if((Get-Item -Path "$($xmls)\$($bglScenery[$i].BaseName).xml").Length -lt 2KB){
+
+    <# if(Test-Path -Path "$($xmls)\$($bglScenery[$i].BaseName).xml"){
+        if((Get-Item -Path "$($xmls)\$($bglScenery[$i].BaseName).xml").Length -lt 1KB){
             Remove-Item "$($xmls)\$($bglScenery[$i].BaseName).xml" -Force -Confirm:$false
         }
-    }
+    } #>
 }
--- SceneriesInfosExtractorMultiThread.ps1
+++ SceneriesInfosExtractorMultiThread.ps1
@@ -1,12 +1,161 @@
-﻿Install-Module SplitPipeline
-cls
+﻿
+
+function GetRunwayAreaValue #Renvoi un equivalent m2 de l'aeroport
+{
+    param (
+        [string]$length,  #"1234.5M" "1234.58Ft"
+        [string]$width
+    )
+    try {
+        $floatLength = 0
+        $floatwidth = 0
+        $area = 0
+
+        $floatLength = DistanceToMeter $length
+        $floatwidth = DistanceToMeter $width
+
+        $area = [int]($floatLength * $floatwidth)
+        
+        return $area  #En M²
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+}
+
+function GetAirportSize
+{
+    param (
+        [int]$RunwayArea,  #En M2
+        [int]$RunwayMaximumLength
+    )
+    try {
+        if($RunwayArea -ge 220000 -and $RunwayMaximumLength -ge 2900)
+        {
+            $size = "Large"
+        } 
+        elseif($RunwayArea -ge 100000 -and $RunwayMaximumLength -ge 2000)
+        {
+            $size = "Medium"
+        } 
+        else
+        {
+            $size = "Small"
+        }
+
+        return $size
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+}
+
+function DistanceToMeter  #Param "1234.45FT" or "1234.14M"
+{
+    param(
+        [string]$distanceString
+    )
+    try {
+        if($distanceString -like "*M")
+        {
+            $floatDistance = [float]($distanceString -replace "M")
+        }
+        elseif($distanceString -like "*FT")
+        {
+            $floatDistance = [float]($distanceString -replace "FT") * 0.3048
+        }
+        else {
+            throw "Not a valid distance"
+        }
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+
+    return $floatDistance
+}
+
+$ImcrementSharedVariable = {
+
+    process {
+        Write-Host "Traitement de: $($_.Local)"
+        $repScenTmp = $($_.Local).Replace("\","_").replace(":","_")
+        $sceneryTemp = "$tempRep\$repScenTmp"
+
+        $bgls = "$($_.Local)\scenery"
+        if(!(Test-Path $bgls.Replace("[","``[").Replace("]","``]"))){
+            Write-Host "$($bgls) DOES NOT EXISTS"
+            return
+        }
+        $folder = New-Item -ItemType Directory -Path $sceneryTemp
+        $xmls = $sceneryTemp.Replace(" ","`` ").Replace("(","``(").Replace(")","``)").Replace("[","``[").Replace("]","``]").Replace(",","``,")
+        
+        $bglsForDocker = $bgls.Replace(" ","`` ").Replace("(","``(").Replace(")","``)").Replace("[","``[").Replace("]","``]").Replace(",","``,")
+
+        Invoke-Expression "docker run --isolation=process --rm -v $($bglToXmlPath):C:\Bgl2Xml -v $($bglsForDocker):C:\bgls -v $($xmls):C:\xmls -v D:\Documents\Sources\P3D_Scripts\Contrainershit:c:\scripts mcr.microsoft.com/windows/servercore:1909 powershell.exe c:\scripts\BGLToXMLContainerEdition.ps1"
+
+        $xmlFiles = @($folder | Get-ChildItem)
+
+        foreach($xmlFile in $xmlFiles)
+        {
+            try {
+                [xml]$xml = ($xmlFile | Get-Content)
+            
+                if($null -ne $xml.FSData.Airport)
+                {
+                    #$xml.FSData.Airport.Runway
+                    $TotalRunwaySurface = 0
+                    $MaxRunwayLength = 0
+
+                    foreach($runway in $xml.FSData.Airport.Runway)
+                    {
+                        $TotalRunwaySurface += GetRunwayAreaValue $runway.length $runway.width
+                        $currentRunwayLength = DistanceToMeter $runway.length
+                        if($currentRunwayLength -gt $MaxRunwayLength)
+                        {
+                            $MaxRunwayLength = $currentRunwayLength
+                        }
+                    }
+                    $airportSize = GetAirportSize $TotalRunwaySurface $MaxRunwayLength
+
+                    $airport = @{
+                        "OACI" = $($xml.FSData.Airport.ident);
+                        "Country" = $($xml.FSData.Airport.country);
+                        "City" = $($xml.FSData.Airport.city);
+                        "Latitude" = [double]$($xml.FSData.Airport.lat);
+                        "Longitude" = [double]$($xml.FSData.Airport.lon);
+                        "Altitude" = $($xml.FSData.Airport.alt);
+                        "Name" = $($xml.FSData.Airport.name);
+                        "State" = $($xml.FSData.Airport.state);
+                        "Bgl_Size" = ((Get-Item -Path "$($bgls.Replace("[","``[").Replace("]","``]"))\$($xmlFile.basename).bgl").Length)/1KB;
+                        "XML_Size" = ($xmlFile.Length)/1KB;
+                        "RunwayArea" = $TotalRunwaySurface;
+                        "AirportSize" = $airportSize
+                    }
+                    $dataAirport.Add($xml.FSData.Airport.ident , $airport)
+                }
+                else {
+                    Remove-Item "$($xmlFile.fullname)" -Force -Confirm:$false
+                }       
+            }
+            catch {
+                Write-Host $PSItem.Exception.Message -ForegroundColor Red
+            }
+        }
+
+        $folder | Remove-Item -Force -Confirm:$false -Recurse
+    }
+}
+
+
+Get-Module SplitPipeline
 
 $tempRep = "$PSScriptRoot\Temp"
 if (!(Test-Path -Path $tempRep)){New-Item -ItemType Directory -Path $tempRep} 
 Get-ChildItem -Path $tempRep | Remove-Item -Force -Confirm:$false -Recurse
 
 $dataAirport = [hashtable]::Synchronized(@{})
-$Throttle = 6
+$Throttle = 25
 
 $pathP3DSceneryDefault = "G:\Prepar3D v4\Scenery"
 $excludeListPath = "ExclusionsRegex.csv"
@@ -28,67 +177,50 @@ foreach ($hash in $sceneryJson.GetEnumerator())
     {
         $filteredScenery.Add($hash.value) | Out-Null
         $i++
-        if($i -ge 100){
+        if($i -ge 10000){
             break
         }
     }
 }
 
-$filteredScenery = $filteredScenery | Sort-Object {Get-Random}
+#$filteredScenery = $filteredScenery | Sort-Object {Get-Random}
 
-$ImcrementSharedVariable = {
+cls
 
-    process {
-        Write-Host "Traitement de: $($_.Local)"
-        $repScenTmp = $($_.Local).Replace("\","_").replace(":","_")
-        $sceneryTemp = "$tempRep\$repScenTmp"
+$filteredScenery | Split-Pipeline -Script $ImcrementSharedVariable  -Variable dataAirport,tempRep,bglToXmlPath -Function GetRunwayAreaValue, DistanceToMeter, GetAirportSize -Count $Throttle
 
-        $bgls = "$($_.Local)\scenery"
-        if(!(Test-Path $bgls)){
-            Write-Host "$($bgls) DOES NOT EXISTS"
-            return
-        }
-        $fol = New-Item -ItemType Directory -Path $sceneryTemp
-        $xmls = $sceneryTemp.Replace(" ","`` ").Replace("(","``(").Replace(")","``)").Replace("[","``[").Replace("]","``]").Replace(",","``,")
-        
-        $bgls = $bgls.Replace(" ","`` ").Replace("(","``(").Replace(")","``)").Replace("[","``[").Replace("]","``]").Replace(",","``,")
+Write-Host "!!--BGL STATS--!!"
+$dataAirport.Values.Bgl_Size | measure -AllStats
+Write-Host "!!--XML STATS--!!"
+$dataAirport.Values.XML_Size | measure -AllStats
 
-        Invoke-Expression "docker run --rm -v $($bglToXmlPath):C:\Bgl2Xml -v $($bgls):C:\bgls -v $($xmls):C:\xmls -v D:\Documents\Sources\P3D_Scripts\Contrainershit:c:\scripts mcr.microsoft.com/dotnet/framework/runtime:4.8 powershell.exe c:\scripts\BGLToXMLContainerEdition.ps1"
+$jsonfile = ConvertTo-Json -InputObject $dataAirport
 
-        $xmlFiles = @($fol | Get-ChildItem)
+$jsonfile | Set-Content -Path "$($tempRep)\airports.json" -Encoding unicode
 
-        foreach($xmlFile in $xmlFiles)
-        {
-            [xml]$xml = Get-Content -Path "$($xmlFile.fullname)"
-            
-            try {
-                if($null -ne $xml.FSData.Airport)
-                {
-                    $airport = @{
-                        "Country" = $($xml.FSData.Airport.country) ;
-                        "City" = $($xml.FSData.Airport.city);
-                        "Latitude" = [double]$($xml.FSData.Airport.lat);
-                        "Longitude" = [double]$($xml.FSData.Airport.lon);
-                        "Altitude" = $($xml.FSData.Airport.alt);
-                        "Name" = $($xml.FSData.Airport.name);
-                        "State" = $($xml.FSData.Airport.state)
-                    }
-                    $dataAirport.Add($xml.FSData.Airport.ident , $airport)
-                }
-                else {
-                    Remove-Item "$($xmlFile.fullname)" -Force -Confirm:$false
-                }       
-            }
-            catch {
-                Write-Host "$($xmlFile.basename) - Not a valid airpot file !"
-            }
-        }
+$kmlTemplate = ".\Map\template.kml" 
 
-        #$folder | Remove-Item -Force -Confirm:$false -Recurse
+$kmlGroups = $dataAirport.Values | Group-Object -Property AirportSize
+
+foreach($group in $kmlGroups)
+{
+    [xml]$kml = Get-Content $kmlTemplate -Encoding utf8
+    
+    #$group.Name
+    #$group.Group
+
+    foreach($airpot in $group.Group)
+    {
+        [xml]$kmlPlacemark = "<Placemark>
+                <name>$($airpot.OACI) - $($airpot.city) - $($airpot.name)</name>
+                <description></description>
+                <Point>
+                <coordinates>$($airpot.Longitude -replace ",","."),$($airpot.Latitude -replace ",",".")</coordinates>
+                </Point>
+            </Placemark>" 
+       $kml.kml.AppendChild($kml.ImportNode($kmlPlacemark.Placemark,$true)) | Out-Null
     }
+
+    $kml.Save("$($tempRep)\$($group.Name)_Airports.kml")
 }
-
-$filteredScenery | Split-Pipeline -Script $ImcrementSharedVariable  -Variable dataAirport,tempRep,bglToXmlPath -Count $Throttle
-
-$dataAirport | fl
 
